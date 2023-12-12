610d442da10d00f0135e4deca089a8ae88aaeaf2
johannes-xerxes-sz <johannes.dalogdog@supportzebra.com>
2023-12-12 10:38:28 +0800

    working basic face_recognition system

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
 import cv2
-import face_recognition
+from simple_facerec import SimpleFacerec
 
-img = cv2.imread("Messi1.webp")
-rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-img_encoding = face_recognition.face_encodings(rgb_img)[0]
+# Encode faces form a folder
+sfr = SimpleFacerec()
+sfr.load_encoding_images("images/")
 
+# Load Camera
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    
+    # Detect Faces
+    face_locations, face_names = sfr.detect_known_faces(frame)
+    for face_loc, name in zip(face_locations, face_names):
+        y1, x2, y2, x1 = face_loc[0], face_loc[1], face_loc[2], face_loc[3]
+        
+        cv2.putText(frame, name, (x1, y1 - 10), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 200), 2)
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 200), 4)
+    
+    cv2.imshow("Frame", frame)
+    
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+    
+cap.release()
+cv2.destroyAllWindows()
